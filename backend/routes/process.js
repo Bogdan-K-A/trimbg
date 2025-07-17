2c83ec93b85d04fb8b9cd4d7499319baa6fb3a1c
Bogdan Kozhoma <kozhoma_b@ukr.net>
2025-07-17 11:18:12 +0300

    заменил на rembg

--- backend/routes/process.js
+++ backend/routes/process.js
@@ -11,12 +11,12 @@ import { promisify } from "util";
 const execFileAsync = promisify(execFile);
 
 // ✅ Выбор пути к rembg в зависимости от платформы
-// const rembgCmd =
-//   process.platform === "win32" ? "C:\\Python310\\Scripts\\rembg.exe" : "rembg";
 const rembgCmd =
-  process.platform === "win32"
-    ? "C:\\Python310\\Scripts\\rembg.exe"
-    : "/home/magystuser/.local/bin/rembg";
+  process.platform === "win32" ? "C:\\Python310\\Scripts\\rembg.exe" : "rembg";
+// const rembgCmd =
+//   process.platform === "win32"
+//     ? "C:\\Python310\\Scripts\\rembg.exe"
+//     : "/home/magystuser/.local/bin/rembg";
 // const rembgCmd = "/home/magystuser/.local/bin/rembg";
 
 const router = express.Router();
